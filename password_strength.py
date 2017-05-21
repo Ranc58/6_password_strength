0ec4af8e9099d5230e36fc87cbb7e23f5fc2d698
Ranc58 <rancvova@gmail.com>
2017-05-21 21:48:28 +0300

    create get_password_strength for check user_pass

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,21 @@
+import re
 
 
-def get_password_strength(password):
-    pass
+def get_password_strength(user_pass):
+    n=0
+    company='StroiInvest'
+    if re.match(r'^\w', user_pass):
+        n += 1
+    if re.match(r'[^@#$]', user_pass):
+        n += 2
+    if re.match(r'[^A_Z]', user_pass):
+        n += 3
+    if company in user_pass:
+        n-=1
+    print(n)
+
 
 
 if __name__ == '__main__':
-    pass
+    user_pass='ddFg5@StroiInvest'
+    get_password_strength(user_pass)
