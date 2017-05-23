1cc1dc8452bf13328db0bc2a7a319d9037be6610
Ranc58 <rancvova@gmail.com>
2017-05-23 19:41:05 +0300

    add print_pass_strength_result and edit black_list_strength_check for separate code

--- password_strength.py
+++ password_strength.py
@@ -1,37 +1,39 @@
 import re
 
 
-def get_symbols_strength(user_pass):
-    strength=0
-    symbols_list=[r'^.*[A-Z]+.*$', r'^.*[a-z]+.*$', r'^.*[@#$%_]+.*$', r'^.*[0-9]+.*$']
+def symbols_strength_check(user_pass):
+    strength = 0
+    symbols_list = [r'^.*[A-Z]+.*$', r'^.*[a-z]+.*$', r'^.*[@#$%_-]+.*$', r'^.*[0-9]+.*$']
     for symbols in symbols_list:
         if re.search(symbols, user_pass):
-            strength+=1
-    print(strength)
+            strength += 2
     return strength
 
 
-def get_len_strength(user_pass):
+def len_strength_check(user_pass):
     strength = 0
-    if len(user_pass) > 8:
+    if len(user_pass) > 10:
         strength += 2
     return strength
 
 
-def get_black_list_strength(user_pass):
+def black_list_strength_check(user_pass):
     strength = 0
-    black_list = ['qwerty', '123456', '123', 'password', 'pass','user']
-    strength -= 12 if list(map(lambda black_pass : user_pass.count(black_pass), black_list)) else strength
-    print(strength)
+    black_list = ['qwerty', '123456', '123', 'password', 'pass', 'user']
+    for password in black_list:
+        if user_pass.count(password):
+            strength -= 3
     return strength
+
+
+def print_pass_strength_result(pass_strength_result):
+    print('Your password has %s/10 balls of strength.'
+          % (pass_strength_result))
 
 
 if __name__ == '__main__':
     user_pass = input('Please enter password: ')
-    total_strength=(get_symbols_strength(user_pass)+
-                    get_len_strength(user_pass)+
-                    get_black_list_strength(user_pass))
-    if total_strength > 0:
-        print('Your password has %s/10 balls of strength.' % (total_strength))
-    if total_strength <= 0:
-        print('Your password in blacklist! Please change it!')
+    pass_strength_result = (symbols_strength_check(user_pass) +
+                            len_strength_check(user_pass) +
+                            black_list_strength_check(user_pass))
+    print_pass_strength_result(pass_strength_result)
