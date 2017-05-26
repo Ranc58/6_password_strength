505cdb3463c5784ba08f3105ceea7e709e3d8de2
Ranc58 <rancvova@gmail.com>
2017-05-26 16:21:08 +0300

    Refactor password_strength.py for minimize and structuring code. Add PassBlackList.txt with popular weakly passwords for check user pass

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Example of script launch on Linux, Python 3.5:
 ```#!bash
 $ python3 password_strength.py
 Please enter password: <password>
-Your password has 4/10 balls of strength.
+Your password has 10/10 points of strength.
 ```
 
 Launching on Win is same
--- password_strength.py
+++ password_strength.py
@@ -1,42 +1,53 @@
 import re
+import getpass
+
+
+def words_strength_check(user_pass):
+    if re.search(r'^.*[A-Za-zА-Яа-я]+.*$', user_pass):
+        return True
 
 
 def symbols_strength_check(user_pass):
-    strength = 0
-    symbols_tuple = (r'^.*[A-ZА-Я]+.*$',
-                     r'^.*[a-zа-я]+.*$',
-                     r'^.*[@#$%_-]+.*$',
-                     r'^.*[0-9]+.*$')
-    for symbols in symbols_tuple:
-        if re.search(symbols, user_pass):
-            strength += 2
-    return strength
+    if re.search(r'^.*[@#$%_-]+.*$', user_pass):
+        return True
+
+
+def digits_strength_check(user_pass):
+    if re.search(r'^.*[0-9]+.*$', user_pass):
+        return True
 
 
 def len_strength_check(user_pass):
-    strength = 0
-    if len(user_pass) > 10:
-        strength += 2
-    return strength
+    min_good_len_for_pass = 10
+    if len(user_pass) >= min_good_len_for_pass:
+        return True
 
 
-def blacklist_strength_check(user_pass):
-    strength = 0
-    blacklist_tuple = ('qwerty', '123456', '123', 'password', 'pass', 'user')
-    for password in blacklist_tuple:
-        if user_pass.count(password):
-            strength -= 2
-    return strength
+def blacklist_strength_check(user_pass, blacklist):
+    blacklist = [password.rstrip('\n') for password in blacklist]
+    for password in blacklist:
+        if user_pass == password:
+            return True
 
 
 def print_pass_strength_result(pass_strength_result):
-    print('Your password has {}/10 balls of strength.'
+    print('Your password has {}/10 points of strength.'
           .format(pass_strength_result))
 
 
 if __name__ == '__main__':
-    user_pass = input('Please enter password: ')
-    pass_strength_result = (symbols_strength_check(user_pass) +
-                            len_strength_check(user_pass) +
-                            blacklist_strength_check(user_pass))
+    user_pass = getpass.getpass('Please enter password: ')
+    with open('PassBlackList.txt') as blacklist:
+        blacklist_strength = blacklist_strength_check(user_pass, blacklist)
+    pass_strength_result = 0
+    if words_strength_check(user_pass):
+        pass_strength_result += 2
+    if symbols_strength_check(user_pass):
+        pass_strength_result += 2
+    if digits_strength_check(user_pass):
+        pass_strength_result += 3
+    if len_strength_check(user_pass):
+        pass_strength_result += 3
+    if blacklist_strength:
+        pass_strength_result -= 2
     print_pass_strength_result(pass_strength_result)
